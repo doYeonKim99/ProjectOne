7d6d7af1ae91e23c7e9b01238a21e583ed1b69d2
Doyeon Kim <ehdus859599@gmail.com>
2016-06-15 21:00:18 +0900

    #1 First Program

---
+++ app/src/main/java/kr/hs/emirim/dok96/projectone/MainActivity.java
@@ -0,0 +1,17 @@
+package kr.hs.emirim.dok96.projectone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+    }
+}
